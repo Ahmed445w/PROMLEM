317706c2303c02c06140385998631230c44e3c05
Ahmed445w <151924702+Ahmed445w@users.noreply.github.com>
2024-09-22 16:25:53 +0300

    Add files via upload

---
+++ PROBLEM/1-10/g.js
@@ -0,0 +1,92 @@
+// function f1( num ) { if( num <=0 ) return true ; else return false }
+//console.log(f1  (5));
+//onsole.log(f1  (0));
+// console.log(f1  (-2));
+
+
+
+
+
+
+ //function f2( num1 , num2 ) {  if( num1 == num2 ) return true  ; else return false }
+//console.log(f2  (4, 8));
+//console.log(f2  (2, 2 ));
+//console.log(f2  (2, "2"));
+
+
+
+
+
+
+//function f3( num ,  num2 , num3 ) {  if( num * num2 > num3  ) return true; else return false }
+//console.log(f3 (0.2, 50, 9) );
+//console.log(f3 (0.9, 1, 2) );
+//console.log(f3 (0.9, 3, 2 ) );
+
+
+
+
+
+
+//function f4( bool ) {    if( bool == true ) return "true" ;   if( bool == false ) return "flase" }
+//console.log(f4  (true));
+//console.log(f4  (false));
+
+
+
+
+
+
+
+//function f5(  name  ) {    if(  name == "Mubashir"  ) return  "Hello, my Love! "  ; else return "Hello," + name + "!" }
+//console.log(f5  ( "Matt" ));
+//console.log(f5  ( "Helen"));
+//console.log(f5  ("Mubashir")); 
+
+
+
+
+
+
+//function f6( num1 , num2 ) { if( num1 == 10  || num2 == 10  | num1 + num2 ==10 )  return true ; else return false }
+//console.log(f6  (9 , 10));
+//console.log(f6  ( 9 , 9 ));
+//console.log(f6  (1 , 9));
+
+
+
+
+
+
+//function f7( str  , str1 ) { if( str.length === str1.length  ) return true ; else return false}
+//console.log(f7  ("ab" , "cd"));
+//console.log(f7  ("ABC", "DE"));
+//console.log(f7  ("hello", "edabit"));
+
+
+
+
+
+
+//function f8( num   ) {    if( num % 100 == 0  ) return true ;else return false }
+//console.log(f8  (1));
+//console.log(f8  (1000));
+//console.log(f8  (100));
+
+
+
+
+
+
+//function f9(age , str ) {   if(  age >= 15 || str ) return true ;else return false }
+//console.log(f9 (14, true));
+//console.log(f9  (14, false));
+//console.log(f9 (16, false));
+
+
+
+//function f10(  str ) {  if( str.length % 2 == 0 ) return true;else return false }
+//console.log(f10  ("apples"));
+//console.log(f10 ("pears"));
+//console.log(f10 ("cherry"));
+
---
+++ PROBLEM/problem 10-20/اال/f.js
@@ -0,0 +1,125 @@
+//function  f1( num) { if ( num % 4 == 0    && num % 100 !=0 )   return  true
+   // else  return false }
+//console.log(  f1 (1990));
+//console.log( f1(1924));
+//console.log( f1(2021));
+//console.log( f1(2000));
+
+
+
+//function  f2( num , num1,  str ) { if ( str == "triangle"   ) { return  num * num1 * 0.5 }
+//if (str == "parallelogram") { return num * num1 } }
+//console.log(  f2 (2, 3, "triangle"));
+//console.log(  f2 (8, 6, "parallelogram") );
+//console.log(  f2 (2.9, 1.3, "parallelogram"));
+
+
+
+
+
+//function  f3( str ) 
+// { if (str == "one"  ) { return 1 }  
+// if (str == "two"  ) { return 2 }  
+// if (str == "three") { return 3 }  
+// if (str == "four" ) { return 4 }  
+// if (str == "five" ) { return 5 }  
+// if (str == "six"  ) { return 6 }  
+// if (str == "seven") { return 7 }  
+// if (str == "eight") { return 8 }  
+// if (str == "nine" ) { return 9 }  
+// if (str == "zero" ) { return 0 }  }
+//
+// console.log(f3("one"));
+// console.log(f3("two"));
+// console.log(f3("nine"));
+
+
+//function f4( str ) 
+//{ if (str == "Darth Vader"	 ) { return  "Luke, I am your father." } 
+ // if (str ==     "Leia"	     ) { return  "Luke, I am your sister." }
+ // if (str ==     "Han"	     ) { return  "Luke, I am your brother in law." } 
+ // if (str ==     "R2D2"	     ) { return  "Luke, I am your 	droid." } }
+ // console.log(f4( "Darth Vader" )  );
+ // console.log(f4(   "Leia"	 )  );
+ // console.log(f4(  "Han"	  )  );
+
+  
+
+//function f5( num ,  ) { if ( num ==3  ) { return 1   }
+//else if (num > 3) { return num -=3  }     
+//let k = num / 2   
+// return k  + 1     }
+//console.log(f5( 3));
+//console.log(f5( 9));
+//console.log(f5( 2));
+ 
+
+
+//function f6( num  , num1 )   { return num  ** num1 }
+//console.log(f6( 5, 5 ) );
+//console.log(f6( 10, 10));
+//console.log(f6(3, 3 ));
+
+//function  f7( str)   { return str.length}
+//console.log(f7( "Hello World")  );
+//console.log(f7( "Edabit")  );
+//console.log(f7("wash your hands!")  );
+
+//function f7(arr ,  str) {
+//   for ( let ind  in arr) {
+  //    if (arr[ind] == str) return ind
+//   }
+ //  
+//}
+//console.log( f7(["hi", "edabit", "fgh", "abc"], "fgh"));
+//console.log( f7(["Red", "blue", "Blue", "Green"], "blue"));
+//console.log( f7(["a", "g", "y", "d"], "d"));
+//console.log(f7(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple"));
+
+
+
+//function f8(arr) {  let result = []
+ //  for ( let i in arr ) {
+ //    result[i] = typeof arr[i]
+ //     
+ //  }
+//   return result
+//}
+//console.log(f8([1, 2, "null", []]));
+//console.log(f8(["214", true, false, 2, 2.15, [], null]));
+//console.log( f8([21.1, "float", "array", ["I am array"], null, true, 214]))
+
+
+//function  f9(str) {
+//let len = 0
+ //for (let i of str ) {
+  // len++
+//   
+ //} 
+//return len
+
+//}
+
+//console.log(f9("Hello World"));
+//console.log(f9("Edabit"));
+//console.log(f9("wash your hands!"));
+ 
+
+
+
+
+
+
+ 
+
+//function f10(arr) { let r =[]
+ //  for (let i in arr){
+ //     r[i] = i + arr[i]
+ //  } 
+ //  return r
+//}
+
+//console.log(f10([0, 0, 0, 0, 0])  ) ;
+//console.log(f10([1, 2, 3, 4, 5])  ) ;
+//console.log(f10([5, 4, 3, 2, 1])  ) ;
+
